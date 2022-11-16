c91c04b693463a4c13a283c4b0cb3a59d37f9ec2
Arnfriduri <32144594+Arnfriduri@users.noreply.github.com>
2022-11-16 19:45:32 +0000

    Fonts, image, and text added to boxes

--- lib/main.dart
+++ lib/main.dart
@@ -6,36 +6,110 @@ void main() {
 
 class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Container(
-                width: 100,
-                height: 100,
-                color: Colors.blue,
-                child: Text('First box'),
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage(
+                  'images/Eg.png',
+                ),
+              ),
+              Text(
+                'Arnfríður Ingvars',
+                style: TextStyle(
+                  fontFamily: 'Oxanium',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
               ),
               Container(
-                width: 100,
-                height: 100,
-                color: Colors.deepOrange,
-                child: Text('The Second box'),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(18),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.teal.shade800,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      '+354 867 8412',
+                      style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
               Container(
-                width: 100,
-                height: 100,
-                color: Colors.green,
-                child: Text('Box three free'),
+                padding: EdgeInsets.all(10),
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 3,
+                  ),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(18),
+                  ),
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade800,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      'arnfriduri@gmail.com',
+                      style: TextStyle(
+                        color: Colors.teal.shade800,
+                        fontFamily: 'Source Sans Pro',
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ]
-          )
-        )
-      )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
